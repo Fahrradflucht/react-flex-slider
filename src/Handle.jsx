fd6cf53eb3fa3e12583e26b33f70c99b84eac83d
Mathis Wiehl <mathis.wiehl@sinnerschrader.com>
2017-10-12 09:27:45 +0200

    fix: Make Handle a class to enable refs
    The slider carries refs to the Handles but the handle was an SFC which doesn't
    support refs.

--- src/Handle.jsx
+++ src/Handle.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Handle = ({ className, offset }) => {
-  const style = { left: `${offset}%` };
-  return <div className={className} style={style} />;
-};
+// This needs to be a class component since the slider carries refs to the
+// handles.
+// eslint-disable-next-line react/prefer-stateless-function
+class Handle extends React.Component {
+  render() {
+    const { className, offset } = this.props;
+    const style = { left: `${offset}%` };
+
+    return <div className={className} style={style} />;
+  }
+}
 
 Handle.propTypes = {
   className: PropTypes.string,
